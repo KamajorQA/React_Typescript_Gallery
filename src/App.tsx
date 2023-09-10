95fad9380be01502114316168dc998dc86bfc921
Kamajor <kamajorqa@gmail.com>
2023-09-10 21:35:39 +0300

    Сделана верстка основного макета. Для стилизации использован Tailwind. Отрендерены статические данные. В продукте реализована работа с состоянием через хук useState. Реализован условный рендеринг и условная смена классов.

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
-import React from 'react';
+import { Product } from './components/Product';
+import { productsData } from './utilities/productsData';
 
 function App() {
-  return <h1>React typescript starter template</h1>;
+  return (
+    <div className="container mx-auto max-w-2xl pt-5">
+      <Product product={productsData[0]} title={'first'} />
+      <Product product={productsData[1]} title={'second'} />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/Product.tsx
@@ -0,0 +1,61 @@
+import { useState } from 'react';
+import { IProduct } from '../models';
+
+interface ProductProps {
+  product: IProduct;
+  title: string;
+}
+
+function Product({ product }: ProductProps) {
+  const [details, setDetails] = useState(false);
+
+  const mainBgColorGradient =
+    'bg-gradient-to-r from-green-400/50 to-blue-500/50';
+  const altBgColorGradient =
+    'bg-gradient-to-l from-indigo-400/50 via-purple-500/50 to-pink-500/50';
+
+  const btnBgClassName = !!details ? altBgColorGradient : mainBgColorGradient;
+
+  const btnClasses = [
+    'py-2 px-4 w-32 border rounded',
+    btnBgClassName,
+    'hover:from-pink-500 hover:to-yellow-500',
+  ];
+
+  return (
+    <div className="border py-2 px-4 rounded flex flex-col items-center mb-4 gap-5">
+      <p className="font-bold w-1/2 text-center"> {product.title}</p>
+
+      <img
+        src={product.image}
+        alt={product.title}
+        className="max-h-48 max-w-xs"
+      />
+      <p className="font-bold"> {`Price: ${product.price} \u00A3`}</p>
+      <button
+        className={btnClasses.join(' ')}
+        onClick={() => setDetails((prevState) => !prevState)}
+      >
+        {!!details ? 'Hide Details' : 'Show Details'}
+      </button>
+      {!!details && (
+        <div className="flex flex-col gap-5 justify-items-center items-center">
+          <p className="text-center">{product.description}</p>
+          <p className="flex gap-5">
+            <span>
+              Rate:
+              <span style={{ fontWeight: 'bold' }}> {product.rating.rate}</span>
+            </span>
+            <span className="opacity-25">|</span>
+            <span>
+              Reviews:{' '}
+              <span style={{ fontWeight: 'bold' }}>{product.rating.count}</span>
+            </span>
+          </p>
+        </div>
+      )}
+    </div>
+  );
+}
+
+export { Product };
